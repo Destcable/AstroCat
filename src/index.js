c07d6f7bacdedc8cbda54971be9c0dc6014733bf
Destcable <artmobpavlov21@gmail.com>
2024-05-06 02:49:29 +0300

    [upd]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import container from "./elements/container";
-import star from "./elements/star";
 import user from "./elements/user";
 import onClickFieldPlay from "./events/onClickFieldPlay";
 import fallUser from "./features/fallUser";
 import starCore from "./features/starCore";
+import store from "./reducer";
 
 const body = document.querySelector('body');
 body.appendChild( container );
@@ -11,7 +11,7 @@ starCore.generate(20);
 container.appendChild( user );
 
 fallUser.waitStop(() => { 
-    console.log('Достигла');
+    console.log('Вы проиграли! Ваш счет: ' + store.getState().stars);
     fallUser.stop();
     starCore.clearAll();
 });
